6be92653b53db9f62784e454c940873e39b91457
denisoliveiraa <denis_o@hotmail.com.br>
2024-02-01 14:59:55 -0300

    add feature to change label when changesize of watch

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const imagemVisualizacao = document.getElementById('imagem-visualizacao')
+const tituloProduto = document.getElementById('titulo-produto')
 const verdeCipreste = {
   nome: 'Verde-cipreste',
   nomePastaImagens: 'imagens-verde-cipreste',
@@ -21,10 +22,26 @@ const rosaClaro = {
   nomePastaImagens: 'imagens-rosa-claro',
 };
 
+let opcoesCores = [verdeCipreste, azulInverno, meiaNoite, estelar, rosaClaro]
+let opcoesTamanho = ['41 mm', '45 mm']
 let imagemSelecionada = 1;
+let tamanhoSelecionado = 1;
 
 function trocarImagem(){
   const idOpcaoSelecionada = document.querySelector('[name="opcao-imagem"]:checked').id
   imagemSelecionada = idOpcaoSelecionada.charAt(0);
   imagemVisualizacao.src = './imagens/opcoes-cores/imagens-azul-inverno/imagem-' + imagemSelecionada + '.jpeg'
+}
+
+function trocarTamanho(){
+  const idOpcaoSelecionada = document.querySelector('[name="opcao-tamanho"]:checked').id
+  tamanhoSelecionado = idOpcaoSelecionada.charAt(0);
+
+  tituloProduto.innerText = "Pulseira loop esportiva azul-inverno para caixa de " + opcoesTamanho[tamanhoSelecionado];
+
+  if(opcoesTamanho[tamanhoSelecionado] === '41 mm'){
+    imagemVisualizacao.classList.add('caixa-pequena')
+  }else {
+    imagemVisualizacao.remove('caixa-pequena')
+  }
 }
